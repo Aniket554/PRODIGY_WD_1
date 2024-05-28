8a82841b4b1e4ad3febcd587b2321fa9afb6044b
Aniket554 <151576054+Aniket554@users.noreply.github.com>
2024-05-28 21:31:01 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+    var sections = document.querySelectorAll('section');
+    var navLinks = document.querySelectorAll('header nav a');
+
+    window.onscroll = () => {
+        sections.forEach(sec => {
+            var top = window.scrollY;
+            var offset = sec.offsetTop - 150;
+            var height = sec.offsetHeight;
+            var id = sec.getAttribute('id');
+
+            if (top >= offset && top < offset + height) {
+                navLinks.forEach(link => {
+                    link.classList.remove('active');
+                });
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            }
+        });
+    };
